d9a1664b7a59d6186d88d971ddb4489127bc33b5
LeoBtk <leonard.benthake@tum.de>
2023-10-28 12:37:08 +0200

    update

--- ATSP-ZKP/src/main.rs
+++ ATSP-ZKP/src/main.rs
@@ -3,10 +3,26 @@ extern crate rand;
 
 use num_bigint::{BigUint, ToBigUint};
 use rand::Rng;
+use std::io;
 
 mod schnorr_zkp;
 mod prover;
 mod verifier;
+
+/*
+fn generate_random_prime(bits: u64) -> BigUint {
+    let mut rng = rand::thread_rng();
+    let prime = num_bigint::BigUint::from_bytes_be(&rng.gen_prime(bits));
+    prime
+}
+
+fn generate_random_generator(p: &BigUint) -> BigUint {
+    let mut rng = rand::thread_rng();
+    let gen = rng.gen_biguint_below(p);
+    gen
+}
+ */
+
 
 fn main() {
     
@@ -14,37 +30,67 @@ fn main() {
 
     //schnorr_zkp::run_age_verification(9);
 
+    println!("Please enter your private key for registration:");
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read input");
     
-    let private_key = 77; // Replace with the actual age (private key)
+    // Parse the input as an integer
+    let private_key: u32 = input.trim().parse().expect("Invalid input.");
+    
+    //let private_key = 77; // Replace with the actual age (private key)
 
     // Convert the private key (age) to a BigUint
     let x = BigUint::from(private_key);
 
     // Define prime number and generator
     let p = BigUint::from(23u32); // A small prime for demonstration
-    let g = BigUint::from(5u32);  // A generator
+    //let g = BigUint::from(5u32);  // A generator
+    let g = BigUint::from(rand::thread_rng().gen::<u32>()); // Random generator value
+    
+
+    /*
+    // Generate random prime number and generator
+    let bits = 128; // Adjust the number of bits as needed
+    let p = generate_random_prime(bits);
+    let g = generate_random_generator(&p);
+     */
+
+    
 
     // Compute the public key
     let y = g.clone().modpow(&x, &p); // Public key
 
-    
+    println!("Public key computed and stored in the server.");
+
+    println!("Press Enter to continue to the Login stage...");
+
+    // Wait for a key press to proceed
+    let mut wait_for_enter2 = String::new();
+    io::stdin().read_line(&mut wait_for_enter2).expect("Failed to read input");
+
+    println!("Please enter your private key for login:");
+
+    let mut input2 = String::new();
+    io::stdin().read_line(&mut input2).expect("Failed to read input");
+
+    // Parse the input as an integer
+    let private_key2: u32 = input2.trim().parse().expect("Invalid input.");
 
     // Generate a random challenge
-    //let c = BigUint::from(42u32); // Replace with the actual challenge
-    let c = verifier::generate_Challenge();
-    //let c_clone = c.clone();
+    let c = verifier::generate_challenge();
     
     // Prover generates the proof
-    let (t, s) = prover::generate_proof(private_key, c.clone(), p.clone(), g.clone(), y.clone());
+    let (t, s) = prover::generate_proof(private_key2, c.clone(), p.clone(), g.clone(), y.clone());
     
     
 
     // Verifier checks the proof
-    let age_threshold = 18; // Age threshold for verification
-    if verifier::verify_proof(p, g, t, s, y, c.clone(), age_threshold) {
-        println!("Age verification 2: Age is valid and above 18.");
+    //let age_threshold = 18; // Age threshold for verification
+    if verifier::verify_proof(p, g, t, s, y, c.clone()) {
+        println!("Proof valid - Login successful!");
     } else {
-        println!("Age verification 2: Age is invalid or below 18.");
+        println!("Proof invalid - Login forbidden!");
     }
     
 }
--- ATSP-ZKP/src/verifier.rs
+++ ATSP-ZKP/src/verifier.rs
@@ -6,14 +6,14 @@ use rand::Rng;
 
 use num_bigint::{BigUint, ToBigUint};
 
-pub fn generate_Challenge() -> BigUint {
+pub fn generate_challenge() -> BigUint {
     let c = BigUint::from(rand::thread_rng().gen::<u32>()); // Random challenge
     (c)
 }
 
-pub fn verify_proof(p: BigUint, g: BigUint, t: BigUint, s: BigUint, y: BigUint, c: BigUint, age_threshold: u32) -> bool {
+pub fn verify_proof(p: BigUint, g: BigUint, t: BigUint, s: BigUint, y: BigUint, c: BigUint) -> bool {
     // Verify that age (private key) is above the specified threshold
-    let age_threshold_big = BigUint::from(age_threshold);
+    //let age_threshold_big = BigUint::from(age_threshold);
 
     let left_side = g.clone().modpow(&s, &p);
     let right_side = t.clone() * y.clone().modpow(&c, &p) % &p;
